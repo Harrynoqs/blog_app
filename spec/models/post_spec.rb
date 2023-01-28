291e5c47b26d1cdda8d152fcf79fa4589a1c1e0b
Harrynoqs <noqsnoqs@gmail.com>
2023-01-28 12:05:12 +0300

    Add test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -18,19 +18,11 @@ RSpec.describe Post, type: :model do
     expect(post).to_not be_valid
   end
 
-  it 'Testing post PosteCounter number negative' do
+  it 'Title should be present' do
     post = Post.create(AuthorId: 1, Title: 'John', Text: 'anything')
-    post.PostCounter = -10
+    post.Title = nil
     expect(post).to_not be_valid
   end
-
-  it 'Testing post LatestPost number greater than 3' do
-    post = Post.create(AuthorId: 1, Title: 'John', Text: 'anything')
-    post.LatestPost = 4
-    expect(post).to_not be_valid
-  end
-
-
 
   it 'Title should not exceed 250 characters' do
     subject.Title = 'a' * 300
