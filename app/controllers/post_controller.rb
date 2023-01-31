e1eda42cebb7bea52e5470cdd5e5ebd87256b631
Harrynoqs <noqsnoqs@gmail.com>
2023-01-31 11:39:56 +0300

    Add index and show methods

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,4 +1,11 @@
 class PostController < ApplicationController
+
+  def index
+    puts :posts
+  end
+
   def view
+    user_id = param[:id].to_i
+    @posts = Post.where(user_id == :author_id)
   end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,10 @@
 class UserController < ApplicationController
-  def view
+  def index
+    puts :users
+  end
+
+  def view    
+    user_id = params[:id].to_i
+    puts User.find(user_id)
   end
 end
