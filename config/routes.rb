9aa8aaf8c6e518df177e6f0af226ae20b5f7f0a3
Harrynoqs <noqsnoqs@gmail.com>
2023-01-31 11:23:21 +0300

    Add controller for post and user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'post/view'
+  get 'user/view'
   get 'pages/blog_app'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
