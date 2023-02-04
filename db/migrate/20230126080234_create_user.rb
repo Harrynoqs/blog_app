0d99aeb689693ce26869a620d30d11235fb7256f
Harrynoqs <noqsnoqs@gmail.com>
2023-02-04 06:03:00 +0300

    Add migration files

--- db/migrate/20230126080234_create_user.rb
+++ db/migrate/20230126080234_create_user.rb
@@ -1,10 +1,10 @@
 class CreateUser < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :Name
-      t.text :Bio
-      t.integer :PostCounter
-      t.string :Image
+      t.string :name
+      t.string :photo
+      t.string :bio
+      t.integer :posts_counter
 
       t.timestamps
     end
--- db/migrate/20230126080452_create_like.rb
+++ db/migrate/20230126080452_create_like.rb
@@ -1,14 +1,7 @@
 class CreateLike < ActiveRecord::Migration[7.0]
   def change
-    create_table :likes do |t|
-      t.integer :AuthorId
-      t.integer :PostId
-
+    create_join_table :users, :posts, table_name: :likes do |t|
       t.timestamps
     end
-    add_foreign_key :likes, :users, column: :AuthorId, primary_key: :id
-    add_index :likes, :AuthorId
-    add_foreign_key :likes, :posts, column: :PostId, primary_key: :id
-    add_index :likes, :PostId
   end
 end
