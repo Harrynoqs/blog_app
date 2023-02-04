abad1b670e6d10572282e6b3c268be6c5cb81493
Harrynoqs <noqsnoqs@gmail.com>
2023-02-04 06:01:19 +0300

    Refactor models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,10 @@
 class Comment < ApplicationRecord
   after_create :update_comments_counter
   belongs_to :post
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
 
   def update_comments_counter
-    Post.find(post.id).increment!(:comments_counter)
+    post_id = post.id
+    Post.increment_counter(:comments_counter, post_id)
   end
 end
