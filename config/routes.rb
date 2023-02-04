660b54c393584dd0abcdc800e715fe073434b88f
Harrynoqs <noqsnoqs@gmail.com>
2023-02-04 23:01:15 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   resources :users, only: [:index,  :show] do
     resources :posts, only: [:index, :show, :new, :create] do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :new]
     end
   resources :posts do 
     resources :likes, only: [:create]
